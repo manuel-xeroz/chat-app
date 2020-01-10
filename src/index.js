f407a6130c96bfbfeb81439763e5f242b68c1497
manuel-xeroz <55798392+manuel-xeroz@users.noreply.github.com>
2020-01-10 23:01:38 +0100

    included env port

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ const { addUser, removeUser, getUser, getUsersInRoom } = require('../src/utils/u
 
 const app = express();
 const server = http.createServer(app)
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 const io = socketio(server)
 const publicDirectoryPath = path.join(__dirname, '../public');
